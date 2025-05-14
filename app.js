0ea83a52476a75cca9e811efec1da73686b267a9
khacdat0511 <khac.dat0511@gmail.com>
2025-05-14 12:09:37 +0700

    done app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/UserRoutes');
 
 var app = express();
 
@@ -13,13 +11,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 const PORT = process.env.PORT || 3000;
 
